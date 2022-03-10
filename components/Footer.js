4308e7fe4d5c8f1c3d6ff04f2f5789418d4ce848
Jr Gnu <91812143+jrgnuproject@users.noreply.github.com>
2022-03-10 16:10:55 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you   <a href="https://4okk5.app.link/mMhHa8sq3nb">Here</a>
       </footer>
     </>
   )
